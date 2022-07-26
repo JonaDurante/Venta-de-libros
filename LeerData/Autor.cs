10264b66c4fe5b46f4aacf70c4156d0d61210d7b
Jonathan <71835980+JonaDurante@users.noreply.github.com>
2022-07-25 22:32:03 -0300

    Add files via upload

---
+++ LeerData/Autor.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace LeerData
+{
+    public class Autor
+    {
+        public int AutorId {get;set;}
+        public string Nombre {get;set;}
+        public string Apellido {get;set;}
+        public string Grado {get;set;}
+        public byte[]? FotoPerfil {get;set;} 
+        public ICollection<LibroAutor> LibroLink {get;set;}
+
+    }
+}
---
+++ LeerData/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Linq;
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new AppVentaLibrosContext())
+                {
+                    var Autor = db.Autor.Single(x => x.AutorId == 7);
+                    if (Autor != null){
+                        db.Remove(Autor);
+                        var estado = db.SaveChanges();
+                        Console.WriteLine("El estado de la transacción es: " + estado);
+                    }
+                }    
+            }
+    }
+}
